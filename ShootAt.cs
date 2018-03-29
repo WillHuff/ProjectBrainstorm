35f3ef73adc3933045b0f1a9fd66b9d8fec564a8
William Huff <30882789+WillHuff@users.noreply.github.com>
2018-03-29 10:24:37 -0500

    Add files via upload

---
+++ ShootAt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootAt : MonoBehaviour {
+	public Rigidbody bullet;
+	public float velocity = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (Fire ());
+	}
+	
+	IEnumerator Fire()
+	{
+		Rigidbody newBullet = Instantiate(bullet,transform.position,transform.rotation) as Rigidbody;
+		newBullet.AddForce(transform.forward*velocity,ForceMode.VelocityChange);
+		yield return new WaitForSeconds (1);
+		StartCoroutine (Fire ());
+	}
+}
